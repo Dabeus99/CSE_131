0921dff447af4cd755eabbdd91c886f3b3a97926
DallinBeus <dbeus99@byui.edu>
2026-09-19 19:33:45 -0400

    let student equal johb

--- week01/ponder/week1.js
+++ week01/ponder/week1.js
@@ -7,8 +7,8 @@ const one = 1;
 const two = '2';             
 
 let course = "CSE131"; //global scope
+let student = "John";
 if (true) {
-    let student = "John";
     console.log(course);  //works just fine, course is global
     console.log(student); //works just fine, it's being accessed within the block
 }
